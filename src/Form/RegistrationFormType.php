24372ac573c0b7cd34ebde971b8f91dfc2d2a217
yann <yann.eysseric@egs.school>
2023-08-16 01:13:38 +0200

    📦 NEW: front formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,55 +5,201 @@ namespace App\Form;
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+// use Symfony\Component\Validator\Constraints\IsTrue;
+// use Symfony\Component\Validator\Constraints\Length;
+// use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('prenom')
-            ->add('nom')
-            ->add('dateNaissance')
-            ->add('adresse')
-            ->add('ville')
-            ->add('codePostal')
+            ->add('pseudo', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un pseudo.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50'
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un Prénom.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un Nom.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label' => 'Date de naissance',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer une date de naissance.'
+                    ])
+                ]
+            ])
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer une adresse.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 255])
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer une ville.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 255])
+                ]
+            ])
+            ->add('codePostal', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un code postal.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
             // ->add('adresseMail')
             // ->add('motDePasse')
             // ->add('estAdmin')
-            ->add('email')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'label' => 'Mot de passe',
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un email.'
                     ]),
-                    new Length([
+                    new Assert\Length(['min' => 2, 'max' => 255])
+                ]
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '255'
+                ],
+                'label' => 'Mot de passe',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe.'
+                    ]),
+                    new Assert\Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Le mot de passe doit faire au moins {{ limit }} caractères.',
                         'max' => 4096,
                     ]),
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'label' => 'Termes d\'accords',
+                'label_attr' => [
+                    'class' => 'form-check-label mx-3'
+                ],
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new Assert\IsTrue([
+                        'message' => 'Vous devez cocher cette case pour continuer.',
                     ]),
                 ],
             ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary rounded-pill btn-lg'
+                ],
+                'label' => 'Envoyer'
+            ]);
         ;
     }
 
