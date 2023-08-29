346b1b870a127cb8be4004e70fb31bf60c318209
Yann <yann.eysseric@egs.school>
2023-08-29 17:32:47 +0200

    📦 NEW: form + front tag (twig et type)

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -6,13 +6,37 @@ use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TagType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+        ->add('nom', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                'minlenght' => '2',
+                'maxlenght' => '10',
+            ],
+            'label' => 'Nom',
+            'label_attr' => [
+                'class' => 'form-label mt-4'
+            ],
+            'constraints' => [
+                new Assert\NotBlank(),
+                new Assert\Length(['min' => 2, 'max' => 50])
+            ]
+        ])
+        ->add('submit', SubmitType::class, [
+            'attr' => [
+                'class' => 'btn btn-primary mt-4 rounded-pill btn-lg'
+            ],
+            'label' => 'Soumettre'
+        ]);
         ;
     }
 
