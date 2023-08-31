5aa967cbab280e45e0e976e165805e9c20ff120d
Yann <yann.eysseric@egs.school>
2023-08-31 20:57:48 +0200

    📦 NEW: form utilisateur

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -6,25 +6,165 @@ use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('prenom')
-            ->add('nom')
-            ->add('dateNaissance')
-            ->add('adresse')
-            ->add('ville')
-            ->add('codePostal')
+        ->add('pseudo', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                'minlength' => '2',
+                'maxlength' => '50',
+            ],
+            'label_attr' => [
+                'class' => 'form-label mt-4'
+            ],
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Veuillez entrer un pseudo.'
+                ]),
+                new Assert\Length(['min' => 2, 'max' => 50])
+            ]
+        ])
+        ->add('prenom', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                'minlength' => '2',
+                'maxlength' => '50'
+            ],
+            'label' => 'Prénom',
+            'label_attr' => [
+                'class' => 'form-label mt-4'
+            ],
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Veuillez entrer un Prénom.'
+                ]),
+                new Assert\Length(['min' => 2, 'max' => 50])
+            ]
+        ])
+        ->add('nom', TextType::class, [
+            'attr' => [
+                'class' => 'form-control',
+                'minlength' => '2',
+                'maxlength' => '50',
+            ],
+            'label_attr' => [
+                'class' => 'form-label mt-4'
+            ],
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Veuillez entrer un Nom.'
+                ]),
+                new Assert\Length(['min' => 2, 'max' => 50])
+            ]
+        ])
+            // ->add('dateNaissance')
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer une adresse.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 255])
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer une ville.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 255])
+                ]
+            ])
+            ->add('codePostal', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un code postal.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
             // ->add('adresseMail')
             // ->add('motDePasse')
             // ->add('estAdmin')
-            ->add('email')
+            // ->add('email')
 //            ->add('roles')
-            ->add('password')
+            // ->add('password')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '255',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un email.'
+                    ]),
+                    new Assert\Length(['min' => 2, 'max' => 255]),
+                    new Assert\Email([
+                        'message' => 'Cette adresse n\'est pas valide'
+                    ])
+                ]
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '255'
+                ],
+                'label' => 'Mot de passe',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe.'
+                    ]),
+                    new Assert\Length([
+                        'min' => 6,
+                        'minMessage' => 'Le mot de passe doit faire au moins {{ limit }} caractères.',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
         ;
     }
 
